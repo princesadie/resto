5120003434b819d9facdbc8bd569a2d67c202a64
Prince Sadie <princesadie@gmail.com>
2016-07-12 22:05:32 -0500

    Basic app

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'slim-rails'
 gem 'faker'
 gem 'json'
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :startup
-  root "startup#index"
+  root to: "startup#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
